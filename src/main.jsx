19509c38e4a43401244a12235e01aa8a2bcdcb60
littlewonny <littlewonny@gmail.com>
2025-04-11 13:24:53 +0900

    v0.1

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WorkoutApp from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WorkoutApp />
+  </React.StrictMode>
+)
